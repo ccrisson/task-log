40d3c489c59049063448e75cb9e58cbf8f24746b
Christopher <crisson.christopher@gmail.com>
2020-12-22 15:05:43 -0500

    mongodb server connected

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Task = require('./models/task');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.l9fhx.mongodb.net/test?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch(() => {
+    console.log("Connection Failed");
+  });
 
 // Middleware for json, headers for cors
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,7 +28,16 @@ app.use((req, res, next) => {
 
 
 app.post('/newtask', (req, res) => {
-  console.log(`ready to add ${JSON.stringify(req.body)} to db`);
+  //console.log(`ready to add ${JSON.stringify(req.body)} to db`);
+  const task = new Task({
+    title: req.body.title,
+    date: req.body.date,
+    content: req.body.content,
+  });
+  task.save();
+  res.status(201).json({
+    message: 'Task added successfully'
+  });
 });
 
 module.exports = app;
